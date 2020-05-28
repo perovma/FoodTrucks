cf63672d64566231204521b5e7c897b2bcc8af25
Mihail Perov <duxatomik@gmail.com>
2020-05-28 12:38:32 +0300

    Edit comment to the script

--- setup-docker-FF.sh
+++ setup-docker-FF.sh
@@ -4,6 +4,10 @@
 docker build -t northernmerlin/foodtrucks-web .
 # create the network
 docker network create foodtrucks
+
+#On the host OS
+#sudo sysctl -w vm.max_map_count=262144
+
 # start the ES container
 docker run -d -p 9200:9200 -p 9300:9300 --net foodtrucks --name es -e "discovery.type=single-node" elasticsearch:7.7.0
 # start the flask app container
