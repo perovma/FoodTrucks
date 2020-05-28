8462c0586feab6cfda85813b79d5a4d00d5ea43c
Mihail Perov <duxatomik@gmail.com>
2020-05-28 12:22:03 +0300

    FoodTruck with ES static site

---
+++ setup-docker-FF.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# build the flask container
+docker build -t northernmerlin/foodtrucks-web .
+# create the network
+docker network create foodtrucks
+# start the ES container
+docker run -d -p 9200:9200 -p 9300:9300 --net foodtrucks --name es -e "discovery.type=single-node" elasticsearch:7.7.0
+# start the flask app container
+docker run -d --net foodtrucks -p 5000:5000 --name foodtrucks-web northernmerlin/foodtrucks-web
